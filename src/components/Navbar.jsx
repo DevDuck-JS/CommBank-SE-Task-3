a8edd31d7f87a647997938217da676d4bcd7df1c
DevDuck-JS <103818112@student.swin.edu.au>
2025-09-04 22:46:08 +1000

    feat: mobile nav-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import Logo from "../assets/CommBankLogo.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,35 +12,149 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab);
 
 const Navbar = () => {
-  return (
-    <div className="flex w-full justify-between items-center shadow-sm bg-white">
-      {/* Desktop */}
-      <div className="hidden lg:flex justify-between items-center ">
-        <img src={Logo} className="w-auto h-10 xl:ms-8 lg:ms-2.5" />
-        <div className="nav-item">Banking</div>
-        <div className="nav-item">Home Loan</div>
-        <div className="nav-item">Insurance</div>
-        <div className="nav-item">Investing & super</div>
-        <div className="nav-item">Business</div>
-        <div className="nav-item">Institutional</div>
-        <div className="nav-item">CommBank Yello</div>
-      </div>
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-      {/* Mobile */}
-      <div className="lg:hidden flex justify-between items-center ">
-        <FontAwesomeIcon icon="fa-solid fa-bars" className="ps-8" />
-        <img src={Logo} className="w-auto h-10 ps-8" />
-      </div>
-      {/* Search & Log on */}
-      <div className="flex justify-end items-center h-20">
-        <div className="flex px-7 border-l-1 border-gray-200 h-full items-center">
-          <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+  return (
+    <div className="relative">
+      {/* Mobile Navbar menu */}
+
+      {showMobileMenu && (
+        <div className="flex items-center bg-[#231F20] fixed top-0 z-700 h-full w-full text-white">
+          <div className="w-full p-6 fixed top-0 ">
+            {/* Close button */}
+            <div className="text-end ">
+              <FontAwesomeIcon
+                icon="fa-solid fa-xmark"
+                className="hover:cursor-pointer"
+                onClick={() => setShowMobileMenu((previousState) => false)}
+              />
+            </div>
+            {/* Top menu */}
+            <ul>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/banking.html?ei=mv_banking">
+                  Banking
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/home-loans.html?ei=mv_home-loans">
+                  Home Loan
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/insurance.html?ei=mv_insurance">
+                  Insurance
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/investing-and-super.html?ei=mv_investing-super">
+                  Investing & super
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/business.html?ei=mv_business">
+                  Business
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/institutional.html?ei=mv_institutional">
+                  Institutional
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/commbank-yello.html?ei=mv_cbyello">
+                  CommBank Yello
+                </a>
+              </li>
+            </ul>
+
+            {/* Divider line */}
+            <div className="bg-white h-0.25 mobile-nav-item"></div>
+            {/* Bottom menu */}
+            <ul>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/digital/locate-us/?ei=mv_locate-us">
+                  Locate us
+                </a>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="https://www.commbank.com.au/support.html?ei=mv_support">
+                  Help & Support
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex bg-[#FFCC00] px-4 min-h-full items-center">
-          <span>
-            <FontAwesomeIcon icon="fa-solid fa-lock" />
-          </span>
-          <span className="font-bold ps-2">Log on</span>
+      )}
+
+      {/* Navbar container */}
+      <div className="flex w-full justify-between items-center top-0 shadow-sm bg-white">
+        {/* Desktop */}
+        <div className="hidden lg:flex justify-between items-center ">
+          <img src={Logo} className="w-auto h-10 xl:ms-8 lg:ms-2.5" />
+          <a
+            href="https://www.commbank.com.au/banking.html?ei=mv_banking"
+            className="nav-item"
+          >
+            Banking
+          </a>
+          <a
+            href="https://www.commbank.com.au/home-loans.html?ei=mv_home-loans"
+            className="nav-item"
+          >
+            Home Loan
+          </a>
+          <a
+            href="https://www.commbank.com.au/insurance.html?ei=mv_insurance"
+            className="nav-item"
+          >
+            Insurance
+          </a>
+          <a
+            href="https://www.commbank.com.au/investing-and-super.html?ei=mv_investing-super"
+            className="nav-item"
+          >
+            Investing & super
+          </a>
+          <a
+            href="https://www.commbank.com.au/business.html?ei=mv_business"
+            className="nav-item"
+          >
+            Business
+          </a>
+          <a
+            href="https://www.commbank.com.au/institutional.html?ei=mv_institutional"
+            className="nav-item"
+          >
+            Institutional
+          </a>
+          <a
+            href="https://www.commbank.com.au/commbank-yello.html?ei=mv_cbyello"
+            className="nav-item"
+          >
+            CommBank Yello
+          </a>
+        </div>
+        {/* Mobile */}
+        <div className="lg:hidden flex justify-between items-center">
+          <FontAwesomeIcon
+            icon="fa-solid fa-bars"
+            className="ps-8 hover:cursor-pointer"
+            onClick={() => setShowMobileMenu((previousState) => !previousState)}
+          />
+          <img src={Logo} className="w-auto h-10 ps-8" />
+        </div>
+        {/* Search & Log on */}
+        <div className="flex justify-end items-center h-20">
+          <div className="flex px-7 border-l-1 border-gray-200 h-full items-center">
+            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+          </div>
+          <div className="flex bg-[#FFCC00] px-4 min-h-full items-center">
+            <span>
+              <FontAwesomeIcon icon="fa-solid fa-lock" />
+            </span>
+            <span className="font-bold ps-2">Log on</span>
+          </div>
         </div>
       </div>
     </div>
